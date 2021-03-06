d92539cc86c54ad51954ed9ca57c8d0c9f4296f8
ukrpas <ukrpas@gmail.com>
2021-03-06 22:19:09 +0200

    starting new user registration flow

--- cypress/integration/checkValidationMessage.spec.js
+++ cypress/integration/checkValidationMessage.spec.js
@@ -1,15 +1,35 @@
 import { basePage } from "../support/pages/basePage";
 import {newUserRegistration} from "../support/pages/newUserRegistration";
 
-it ('check the messages if nothing is added on new user registration', () => {
+beforeEach('setup success response', () => {
+    cy.intercept('https://metrics.api.drift.com/monitoring/metrics/widget/init/v2', {fixture: 'successRegistration.json'})
+    
+})
+
+it ('check the blur if nothing is added on new user registration', () => {
 
     basePage.open('https://insider.drift.com/register/free/')
 
     cy.get('.mepr-submit').click();
     newUserRegistration.checkBlurOnRegistration();
+
+})
    
     //checking for messages if empty inputs
+it ('check for messages if empty inputs', () => {
 
     newUserRegistration.checkForMessages();
+
+})
+
+it.only ('register new user', () => {
+    basePage.open('https://insider.drift.com/register/free/')
+    cy.get('[name="user_first_name"').type('testFirstName');
+    cy.get('[name="user_email"').type('test@test.com');
+    cy.get('[name="user_last_name').type('testLastName');
+    cy.get('[name="mepr_user_password"]').type('1');
+    cy.get('[name="mepr_user_password_confirm"]').type('1')
+    cy.get('.mepr-submit').click();
+
 
    })

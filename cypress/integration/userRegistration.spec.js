859007d66d8313adb179542a919290596ce1d8fb
ukrpas <ukrpas@gmail.com>
2021-03-07 00:36:08 +0200

    added new user registration flow

--- cypress/integration/userRegistration.spec.js
+++ cypress/integration/userRegistration.spec.js
@@ -1,16 +1,11 @@
 import { basePage } from "../support/pages/basePage";
 import {newUserRegistration} from "../support/pages/newUserRegistration";
 
-beforeEach('setup success response', () => {
-    cy.intercept('https://metrics.api.drift.com/monitoring/metrics/widget/init/v2', {fixture: 'successRegistration.json'})
-    
-})
 
 it ('check the blur if nothing is added on new user registration', () => {
 
     basePage.open('https://insider.drift.com/register/free/')
-
-    cy.get('.mepr-submit').click();
+    basePage.clickOnCreateNewAccount();
     newUserRegistration.checkBlurOnRegistration();
 
 })
@@ -18,18 +13,16 @@ it ('check the blur if nothing is added on new user registration', () => {
     //checking for messages if empty inputs
 it ('check for messages if empty inputs', () => {
 
+    basePage.open('https://insider.drift.com/register/free/')
+    basePage.clickOnCreateNewAccount();
     newUserRegistration.checkForMessages();
 
 })
 
 it.only ('register new user', () => {
     basePage.open('https://insider.drift.com/register/free/')
-    cy.get('[name="user_first_name"').type('testFirstName');
-    cy.get('[name="user_email"').type('test@test.com');
-    cy.get('[name="user_last_name').type('testLastName');
-    cy.get('[name="mepr_user_password"]').type('1');
-    cy.get('[name="mepr_user_password_confirm"]').type('1')
-    cy.get('.mepr-submit').click();
+    newUserRegistration.newUserRegistrationFlow();
+    cy.wait(2000)
+    basePage.clickOnCreateNewAccount();
 
-
-   })
+})

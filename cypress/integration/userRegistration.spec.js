5039463eb688a4fd3650f4cb7209fa593b6a038c
ukrpas <ukrpas@gmail.com>
2021-03-07 19:03:40 +0200

    added test for mismatch password and refactored tests

--- cypress/integration/userRegistration.spec.js
+++ cypress/integration/userRegistration.spec.js
@@ -7,22 +7,27 @@ it ('check the blur if nothing is added on new user registration', () => {
     basePage.open('https://insider.drift.com/register/free/')
     basePage.clickOnCreateNewAccount();
     newUserRegistration.checkBlurOnRegistration();
-
 })
-   
-    //checking for messages if empty inputs
+       //checking for messages if empty inputs
 it ('check for messages if empty inputs', () => {
 
     basePage.open('https://insider.drift.com/register/free/')
     basePage.clickOnCreateNewAccount();
     newUserRegistration.checkForMessages();
+})
+
+it ('register new user', () => {
+
+    basePage.open('https://insider.drift.com/register/free/')
+    newUserRegistration.newUserRegistrationFlow('testFirstName', 'testLastname', 'test@test.com', '1', '1');
+    basePage.clickOnCreateNewAccount();
 
 })
 
-it.only ('register new user', () => {
+it.only ('check if password is mismatch', () => {
     basePage.open('https://insider.drift.com/register/free/')
-    newUserRegistration.newUserRegistrationFlow();
-    cy.wait(2000)
+    newUserRegistration.newUserRegistrationFlow('testFirstName', 'testLastname', 'test@test.com', '1', '2');
     basePage.clickOnCreateNewAccount();
-
+    cy.get('[name="mepr_user_password_confirm"]').focus().blur();
+    cy.get('.mepr-form-has-errors').should('have.text', 'Please fix the errors above')
 })

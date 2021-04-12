9a690323a2f8a625d598d1d13aa41a21ec9cdce9
ukrpas <ukrpas@gmail.com>
2021-04-12 23:30:38 +0300

    refactored tests for  baseUrl is in beforeEach

--- cypress/integration/userRegistration.spec.js
+++ cypress/integration/userRegistration.spec.js
@@ -5,32 +5,30 @@ describe('new user registration', () => {
 
     beforeEach(() => {
          cy.clearData();
+         basePage.open('register/free/')
     })
 
 it ('check the blur if nothing is added on new user registration', () => {
 
-    basePage.open('register/free/')
     basePage.clickOnCreateNewAccount();
     newUserRegistration.checkBlurOnRegistration();
 })
        //checking for messages if empty inputs
 it ('check for messages if empty inputs', () => {
-
-    basePage.open('register/free/')
+    
     basePage.clickOnCreateNewAccount();
     newUserRegistration.checkForMessages();
 })
 
 it ('register new user', () => {
-
-    basePage.open('register/free/')
+   
     newUserRegistration.newUserRegistrationFlow('testFirstName', 'testLastname', 'test@test.com', '1', '1');
     basePage.clickOnCreateNewAccount();
 
 })
 
 it ('check for Error message if password is mismatch and confirm password input has blur', () => {
-    basePage.open('register/free/')
+   
     newUserRegistration.newUserRegistrationFlow('testFirstName', 'testLastname', 'test@test.com', '1', '2');
     basePage.clickOnCreateNewAccount();
     newUserRegistration.checkForMismatchPass()
